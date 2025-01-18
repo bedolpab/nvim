e50229a4643436801ea1f95fa36416e0626245b8
Pablo Bedolla <bedolpablo@gmail.com>
2025-01-18 17:34:24 -0600

    fix: small updates

--- plugin/packer_compiled.lua
+++ plugin/packer_compiled.lua
@@ -99,6 +99,11 @@ _G.packer_plugins = {
     path = "/Users/pablobedolla/.local/share/nvim/site/pack/packer/start/friendly-snippets",
     url = "https://github.com/rafamadriz/friendly-snippets"
   },
+  ["markdown-preview.nvim"] = {
+    loaded = true,
+    path = "/Users/pablobedolla/.local/share/nvim/site/pack/packer/start/markdown-preview.nvim",
+    url = "https://github.com/iamcco/markdown-preview.nvim"
+  },
   ["modus-themes.nvim"] = {
     loaded = true,
     path = "/Users/pablobedolla/.local/share/nvim/site/pack/packer/start/modus-themes.nvim",
